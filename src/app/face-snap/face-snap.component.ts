d38fcf046c33b2a87a2c978707f61d1f112bd8f5
Anne Walch <anne.walch@yahoo.fr>
2024-11-25 19:24:48 +0100

    initialion du projet

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  creadetAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+ngOnInit(): void {
+  this.title = 'go go girl!';
+  this.description = 'This is the true story of me awakening from my fucking night';
+  this.creadetAt = new Date();
+  this.snaps = 0;
+  this.imageUrl = 'https://www.galerie-com.com/grand_img/0184871001526213614.jpeg'
+}
+}
